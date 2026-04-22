2ddad6bea68f2019beb974a5351890c9392a26ae
jeevesh56 <jeevesh2910@gmail.com>
2026-04-22 10:14:02 +0530

    db1

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.mount("/uploads", StaticFiles(directory=UPLOAD_FOLDER), name="uploads")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
